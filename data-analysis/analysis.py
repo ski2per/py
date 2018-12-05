214dace101af114e9325290ec394e339a6bd3b2c
Ted <ski2per@163.com>
2018-12-05 11:50:33 +0800

    analysis dev

--- data-analysis/analysis.py
+++ data-analysis/analysis.py
@@ -4,29 +4,34 @@ import re
 import os.path
 import pandas as pd
 
-sample_dir = os.path.join(os.path.dirname(__file__), "sample")
+SAMPLE_DIR = os.path.join(os.path.dirname(__file__), "sample")
 
-samples = os.listdir(sample_dir)
-
-total = pd.DataFrame()
-
-for sample in samples:
-    delay = ""
-    mat = re.search("[0-9]+ms", sample)
-    if mat:
-        delay = mat.group(0)
-        
-    df = pd.read_json(os.path.join(sample_dir,sample), lines=True)
-    df["elapse"] = df["endTime"] - df["startTime"]
-    #df.drop(["endTime", "startTime", "invokeStatus", "valueAfter", "valueBefore"],axis=1)
-    total = pd.concat([total, df["elapse"]], axis=1)
-    total = total.rename(columns={"elapse", delay})
-
-print(total)
+def get_sample_list():
+    return os.listdir(SAMPLE_DIR)
 
 
+def plot_all_sample():
+    all = pd.DataFrame()
+    samples = get_sample_list()
+    
+    for sample in samples:
+        delay = ""
+        #mat = re.search("[0-9]+ms", sample)
+        mat = re.search("[0-9]+", sample)
+        if mat:
+            delay = mat.group(0)
 
+        df = pd.read_json(os.path.join(SAMPLE_DIR,sample), lines=True)
+        df["elapse"] = df["endTime"] - df["startTime"]
+        #df.drop(["endTime", "startTime", "invokeStatus", "valueAfter", "valueBefore"],axis=1)
+        all = pd.concat([all, df["elapse"]], axis=1)
+        all = all.rename(columns={"elapse": delay})
 
+    all.plot()
+    plt.show()
 
-#df.plot()
-#plt.show()
+def plot_all_mean():
+    pass
+
+if __name__ == "__main__":
+    plot_all_sample()
