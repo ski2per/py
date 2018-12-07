6d607d0ee4ec7c75227c24a74a4a65b1070707f9
zhangpeng <ski2per@163.com>
2018-12-07 16:43:32 +0800

    network simulation test

--- data-analysis/analysis.py
+++ data-analysis/analysis.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
 
 import re
 import os.path
 import pandas as pd
 
-#SAMPLE_DIR = os.path.join(os.path.dirname(__file__), "sample")
-SAMPLE_DIR = os.path.join(os.path.dirname(__file__), "raw")
+# SAMPLE_DIR = os.path.join(os.path.dirname(__file__), "sample")
+SAMPLE_DIR = os.path.join(os.path.dirname(__file__), "raw2")
+
+font = fm.FontProperties(fname="/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc")
 
 
 def get_sample_list():
@@ -38,7 +41,14 @@ def plot_all_sample():
         all_sample = pd.concat([all_sample, df["elapse"]], axis=1)
         all_sample = all_sample.rename(columns={"elapse": delay})
 
-    all_sample.plot()
+
+    # all_sample.columns = all_sample.columns.astype('int64')
+    # all_sample[:5].plot(sort_columns=True)
+    all_sample = all_sample.reindex_axis(sorted(all_sample.columns), axis=1)
+    # all_sample[:50].plot(ylim=(0, 15), subplots=True)
+    all_sample[:200].plot(ylim=(0, 15), subplots=True, layout=(10,2))
+
+
     plt.show()
 
 
@@ -62,11 +72,16 @@ def plot_all_mean():
     # print(s.index)
     s.index = s.index.astype("int64")
     # s.sort_index().plot.bar()
-    s.sort_index().plot()
+    ax = s.sort_index().plot(x="hello")
+    ax.set_xlabel("网络延时(毫秒)", fontproperties=font)
+    ax.set_ylabel("平均交易成功时间(秒)", fontproperties=font)
+
+    print(s.sort_index())
     plt.show()
 
 
 
+
 if __name__ == "__main__":
-    # plot_all_sample()
-    plot_all_mean()
+    plot_all_sample()
+    # plot_all_mean()
