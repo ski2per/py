e8f9d41b9d041a27fbf160f6e938d9583f07ac01
Ted <ski2per@163.com>
2018-12-12 01:32:43 +0800

    update test2.py

--- data-analysis/test2.py
+++ data-analysis/test2.py
@@ -21,24 +21,40 @@ fibo(1)
 import requests
 import pandas
 from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+
 response=requests.get('http://haoshu.xshcs.com')
 response.encoding='utf-8'
 html=response.text
 strhtml=BeautifulSoup(html,'lxml')
 lists=strhtml.select('[class="title"]')
 lists2=strhtml.select('[class="price"]')
+lists3=strhtml.select('[class="publisher"]')
 
 df = pandas.DataFrame()
 i=0
 for fr in lists:
-    context = fr.string+ '：'+ lists2[i].string.replace('¥','')
+    context = fr.string+ '：'+ lists2[i].string.replace('¥','') + '：' + lists3[i].string
     #print(context)
     i=i+1
-    tmp = pandas.DataFrame([context.split('：')], columns=["书名", "价格"])
+    tmp = pandas.DataFrame([context.split('：')], columns=["书名", "价格", "出版社"])
     df = df.append(tmp, ignore_index=True)
 
-#print(df)
-df.to_csv('new_book.csv', encoding='gbk', index=False)
+# print(df)
+#df.to_csv('new_book.csv', encoding='gbk', index=False)
+
+new_df = df.groupby("出版社").size().to_frame("count")
+#print(new_df)
+font = fm.FontProperties(fname="C:\Windows\Fonts\simhei.ttf")
+fig , ax = plt.subplots()
+_, texts, autotexts = ax.pie(new_df['count'], labels=new_df.index, autopct='%.2f')
+fig.set_figheight(10)
+fig.set_figwidth(10)
+plt.setp(autotexts, fontproperties=font)
+plt.setp(texts, fontproperties=font)
+
+plt.show()
 
 
 
