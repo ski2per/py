72f60f00bc4cc1c28f6134e1bf4f38d028b91a40
Ted <ski2per@163.com>
2018-12-19 11:10:30 +0800

    merge home and office

--- spider/code4-1.py
+++ spider/code4-1.py
@@ -2,25 +2,21 @@ import requests
 import lxml.html
 
 # 获取中国天气网天气预报网页
-http_response = requests.get("http://www.weather.com.cn/weather40d/101270101.shtml")
+http_response = requests.get("http://www.weather.com.cn/weather1d/101270101.shtml")
 # 设置中文编码
 http_response.encoding = "utf-8"
 
-# 将获取的网页HTML保存到4-1.html文件中
-with open("4-1.html", "w", encoding="utf-8") as html:
-    html.write(http_response.text)
+html = lxml.html.fromstring(http_response.text)
 
-with open("4-1.html", "r", encoding="utf-8") as f:
-    html = lxml.html.fromstring(f.read())
-    weather40 = html.xpath('//div[@class="w_xian"]/p')
-    print(weather40)
+# 获取白天温度数据所在的HTML元素(这里是叫span的元素)
+day_temperature_span = html.xpath('//*[@id="today"]/div[2]/ul/li[1]/p[2]/span')
+# 获取夜间温度数据所有的HTML元素
+night_temperature_span =  html.xpath('//*[@id="today"]/div[2]/ul/li[2]/p[2]/span')
 
+# 获取白天温度数据
+day_temperature = day_temperature_span[0].text_content()
+# 获取夜间温度数据
+night_temperature = night_temperature_span[0].text_content()
 
-#print(type(http_response.text))
-#html = lxml.html.fromstring(http_response.text)
-## weather40 = html.xpath('//div[@class="w_xian"]/p')
-#tmp = html.xpath('//*[@id="table"]/tbody/tr[2]/td[1]/h2/span[2]')
-#print(tmp[0])
-#print(tmp[0].text_content())
-
-# print(weather40)
+# 将温度数据格式化之后打印输出
+print('{}°C ~ {}°C'.format(day_temperature, night_temperature))
