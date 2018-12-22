d180fe28bd84d21700a25d446ffc5bdb6c228631
zhangpeng <ski2per@163.com>
2018-12-22 11:19:55 +0800

    change format

--- data-analysis/analysis.py
+++ data-analysis/analysis.py
@@ -6,20 +6,21 @@ import os.path
 import pandas as pd
 
 # SAMPLE_DIR = os.path.join(os.path.dirname(__file__), "sample")
-SAMPLE_DIR = os.path.join(os.path.dirname(__file__), "raw2")
+# SAMPLE_DIR = os.path.join(os.path.dirname(__file__), "delay-loss1-single-pyhsrv-4org3peer-kafka")
+SAMPLE_DIR = os.path.join(os.path.dirname(__file__), "loss-single-pyhsrv-2org2peer-solo")
 
-# font = fm.FontProperties(fname="/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc")
+font = fm.FontProperties(fname="/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc")
 # font = fm.FontProperties(fname="C:\Windows\Fonts\simsun.ttc")
-font = fm.FontProperties(fname="C:\Windows\Fonts\simhei.ttc")
+# font = fm.FontProperties(fname="C:\Windows\Fonts\simhei.ttc")
 
 
-def get_sample_list():
+def get_sample_files():
     return os.listdir(SAMPLE_DIR)
 
 
-def get_delay(sample):
+def get_delay(file_name):
     # mat = re.search("[0-9]+ms", sample)
-    mat = re.search("[0-9]+", sample)
+    mat = re.search("[0-9]+", file_name)
     if mat:
         delay = mat.group(0)
     else:
@@ -30,7 +31,7 @@ def get_delay(sample):
 
 def plot_all_sample():
     all_sample = pd.DataFrame()
-    samples = get_sample_list()
+    samples = get_sample_files()
 
     for sample in samples:
         delay = get_delay(sample)
@@ -63,7 +64,7 @@ def plot_all_sample():
 def plot_all_mean():
     sample_mean = {}
 
-    samples = get_sample_list()
+    samples = get_sample_files()
 
     for sample in samples:
         delay = get_delay(sample)
@@ -88,9 +89,23 @@ def plot_all_mean():
     plt.show()
 
 
+def plot_single(file_name, percent):
+    df = pd.read_json(os.path.join(SAMPLE_DIR, file_name), lines=True)
+    df["elapse"] = df["endTime"] - df["startTime"]
+    ax = df["elapse"].plot()
+    ax.set_title("丢包率：{}".format(percent), fontproperties=font)
+    ax.set_xlabel("交易数量", fontproperties=font)
+    ax.set_ylabel("成功交易平均耗时(秒)", fontproperties=font)
+    plt.show()
 
 
 
 if __name__ == "__main__":
     # plot_all_sample()
-    plot_all_mean()
+    # plot_all_mean()
+    # plot_single("simulation-no-loss.log", "0")
+    # plot_single("simulation-0.01-loss.log", "1%")
+    # plot_single("simulation-0.1-loss.log", "10%")
+    # plot_single("simulation-0.05-loss.log", "5%")
+    # plot_single("simulation-0.03-loss.log", "3%")
+    plot_single("simulation-0.01-loss.log", "1%")
