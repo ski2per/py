c23ba68b7d1d1d9e2b398552feb0ed10c518201b
Ted <ski2per@163.com>
2019-01-04 00:28:02 +0800

    logical bug fix

--- data-analysis/network-simulation.py
+++ data-analysis/network-simulation.py
@@ -1,57 +1,81 @@
 #!/usr/bin/python3
 import re
 import time
+from time import localtime, strftime
 import json
 import subprocess
-from subprocess import Popen,PIPE
+from subprocess import Popen, PIPE
 
-def get_value():
-    #cmd = 'peer chaincode query -C composerchannel -n cc -c \'{"Args":["query","a"]}\''
-    cmd = 'peer chaincode query -C composerchannel -n mycc -c \'{"Args":["query","a"]}\''
+
+
+def write_log(rnd, msg):
+    log_time = strftime("%Y%m%d %H:%M:%S", localtime())
+    log = "[{}](Round {}): {}".format(log_time, rnd, msg)
+    print(log)
+    
+    
+def query_chaincode():
+    cmd = 'peer chaincode query -C mychannel -n mycc -c \'{"Args":["query","a"]}\''
+    # cmd = 'peer chaincode query -C composerchannel -n mycc -c \'{"Args":["query","a"]}\''
 
     pipe = subprocess.run(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-    result =  pipe.stdout.decode().strip()
-    value = result.split(":")[1]
-    return int(value)
+    std_out = pipe.stdout.decode().strip()
+    std_err = pipe.stderr.decode().strip()
+
+    if std_out:
+        value = re.match('.+?\s*:\s*(\d+)', std_out)
+        return value.group(1)
+    else:
+        error = re.match('error.+', std_err, re.I)
+        return error.group(0)
+
 
 def invoke_chaincode():
-    #cmd = 'peer chaincode invoke -o orderer1.example.com:7050 -C composerchannel -n cc -c \'{"Args":["invoke","a","b","1"]}\''
-    cmd = 'peer chaincode invoke -o orderer1.example.com:7050 -C composerchannel -n mycc -c \'{"Args":["invoke","a","b","1"]}\''
+    cmd = 'peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n mycc -c \'{"Args":["invoke","a","b","1"]}\''
+    # cmd = 'peer chaincode invoke -o orderer1.example.com:7050 -C composerchannel -n mycc -c \'{"Args":["invoke","a","b","1"]}\''
     pipe = subprocess.run(cmd, shell=True, stdout=PIPE, stderr=PIPE)
 
     result = pipe.stderr.decode()
     status = re.findall('status:[0-9]+', result)
     return status
 
-def get_log_line():
+
+def test(rnd):
     log_line = {}
 
     start_time = time.time()
-    value_before = get_value()
-    invoke_status = invoke_chaincode()
-    value_after = get_value()
-    
-    while value_before == value_after :
-        value_after = get_value()
-    
+        
+    value_before = query_chaincode()
+    while not value_before.isdigit():
+        write_log(rnd, value_before)
+        value_before = query_chaincode()
+    print(value_before)
+    #invoke_status = invoke_chaincode()
+    #value_after = query_chaincode()
+
+    #while value_before == value_after:
+    #    value_after = query_chaincode()
+
     end_time = time.time()
 
-    log_line["startTime"] = start_time       
-    log_line["valueBefore"] = value_before
-    log_line["invokeStatus"] = invoke_status
-    log_line["valueAfter"] = value_after
-    log_line["endTime"] = end_time
-    
-    return json.dumps(log_line)
-    
+    #log_line["startTime"] = start_time
+    #log_line["valueBefore"] = value_before
+    #log_line["invokeStatus"] = invoke_status
+    #log_line["valueAfter"] = value_after
+    #log_line["endTime"] = end_time
+
+    #return json.dumps(log_line)
 
 
 if __name__ == "__main__":
-    
-    #while True:
-    with open("simulation.log", "w+") as log:
-        i = 0
-        while i < 200:
-            line = get_log_line()
-            print(line, file=log, flush=True)
-            i += 1
+
+    #query_chaincode()
+    test(1)
+
+    #with open("data.log", "w+") as data, \
+    #        open("simulation.log", "w+") as log:
+    #    i = 0
+    #    while i < 200:
+    #        line = get_log_line()
+    #        print(line, file=log, flush=True)
+    #        i += 1
