b5bdf51d2386d5efdbe24460c1cb01f56ae2bc03
zhangpeng <ski2per@163.com>
2019-01-04 20:54:22 +0800

    new simulation

--- data-analysis/network-simulation.py
+++ data-analysis/network-simulation.py
@@ -10,72 +10,100 @@ from subprocess import Popen, PIPE
 
 def write_log(rnd, msg):
     log_time = strftime("%Y%m%d %H:%M:%S", localtime())
-    log = "[{}](Round {}): {}".format(log_time, rnd, msg)
+    log = "[{}]<Round {}>: {}".format(log_time, rnd, msg)
     print(log)
     
     
 def query_chaincode():
-    cmd = 'peer chaincode query -C mychannel -n mycc -c \'{"Args":["query","a"]}\''
+    #cmd = 'peer chaincode query -C mychannel -n mycc -c \'{"Args":["query","a"]}\''
+    cmd = 'peer chaincode query -C mychannel -n aaa -c \'{"Args":["query","a"]}\''
     # cmd = 'peer chaincode query -C composerchannel -n mycc -c \'{"Args":["query","a"]}\''
 
     pipe = subprocess.run(cmd, shell=True, stdout=PIPE, stderr=PIPE)
     std_out = pipe.stdout.decode().strip()
+    #print("stdout:", std_out)
     std_err = pipe.stderr.decode().strip()
+    #print("stderr:", std_err)
 
     if std_out:
         value = re.match('.+?\s*:\s*(\d+)', std_out)
         return value.group(1)
     else:
-        error = re.match('error.+', std_err, re.I)
-        return error.group(0)
+        try:
+            error = re.search('error.+', std_err, re.I)
+            return error.group(0)
+        except AttributeError as err:
+            print("UNKNOWN ERR IN query_chaincode(): {}".format(err))
+            print(error)
+            return "UNKOWN"
+
+def query_till_success(rnd):
+    value = query_chaincode()
+    while not value.isdigit():
+        # If query value is not digit, then write log
+        write_log(rnd, value)
+        value = query_chaincode()
+    value = int(value)
+    return value
 
 
 def invoke_chaincode():
-    cmd = 'peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n mycc -c \'{"Args":["invoke","a","b","1"]}\''
-    # cmd = 'peer chaincode invoke -o orderer1.example.com:7050 -C composerchannel -n mycc -c \'{"Args":["invoke","a","b","1"]}\''
+    cmd = 'peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n aaa -c \'{"Args":["invoke","a","b","1"]}\''
+    #cmd = 'peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n mycc -c \'{"Args":["invoke","a","b","1"]}\''
     pipe = subprocess.run(cmd, shell=True, stdout=PIPE, stderr=PIPE)
 
-    result = pipe.stderr.decode()
-    status = re.findall('status:[0-9]+', result)
-    return status
+    std_out = pipe.stdout.decode()
+    #print("stdout:", std_out)
+    std_err = pipe.stderr.decode()
+    #print("stderr:", std_err)
+
+    status = re.search('success.+status.+?(\d+)', std_err, re.I)
+    if status:
+        return status.group(1)
+    else:
+        try:
+            error = re.search('error:.+', std_err, re.I)
+            return error.group(0)
+        except AttributeError as err:
+            print("UNKNOWN ERR IN invoke_chaincode(): {}".format(err))
+            print(error)
+            return "UNKOWN"
 
 
 def test(rnd):
-    log_line = {}
+    test_info = {}
 
     start_time = time.time()
         
-    value_before = query_chaincode()
-    while not value_before.isdigit():
-        write_log(rnd, value_before)
-        value_before = query_chaincode()
-    print(value_before)
-    #invoke_status = invoke_chaincode()
-    #value_after = query_chaincode()
+    # ====== Get value before invoking ======
+    value_before = query_till_success(rnd)
+    write_log(rnd, "Query Value Before Success: {}".format(value_before))
 
-    #while value_before == value_after:
-    #    value_after = query_chaincode()
+    # ====== Invoke chaincode till success ======
+    status = invoke_chaincode()
+    while not status.isdigit():
+        write_log(rnd, status)
+        status = invoke_chaincode()
 
+    # ====== Get value till value changed ======
+    value_after = query_till_success(rnd)
+    while value_before == value_after:
+        value_after = query_till_success(rnd)
+    
     end_time = time.time()
+    
+    test_info["round"] = rnd
+    test_info["startTime"] = start_time
+    test_info["valueBefore"] = value_before
+    test_info["valueAfter"] = value_after
+    test_info["endTime"] = end_time
 
-    #log_line["startTime"] = start_time
-    #log_line["valueBefore"] = value_before
-    #log_line["invokeStatus"] = invoke_status
-    #log_line["valueAfter"] = value_after
-    #log_line["endTime"] = end_time
-
-    #return json.dumps(log_line)
-
+    return json.dumps(test_info)
 
 if __name__ == "__main__":
-
-    #query_chaincode()
-    test(1)
-
-    #with open("data.log", "w+") as data, \
-    #        open("simulation.log", "w+") as log:
-    #    i = 0
-    #    while i < 200:
-    #        line = get_log_line()
-    #        print(line, file=log, flush=True)
-    #        i += 1
+    with open("data.log", "w+") as data:
+        i = 0
+        while True:
+            line = test(i)
+            print(line, file=data, flush=True)
+            i += 1
