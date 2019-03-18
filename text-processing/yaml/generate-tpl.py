33a4757f758783302c3c3a07973262a16713c929
zhangpeng <ski2per@163.com>
2019-03-18 11:30:13 +0800

    fabric template generator

--- text-processing/yaml/generate-tpl.py
+++ text-processing/yaml/generate-tpl.py
@@ -1,12 +1,14 @@
 import yaml
+from jinja2 import Environment, FileSystemLoader, TemplateNotFound
 
-from jinja2 import Environment, FileSystemLoader
+with open('crypto-config.yaml', 'r') as conf_fd, \
+     open('stack.yaml', 'w') as stack_fd:
+    config_data = yaml.load(conf_fd, yaml.Loader)
 
-with open('crypto-config.yaml', 'r') as f:
-    config = yaml.load(f, yaml.Loader)
-
-env = Environment(loader=FileSystemLoader('./'), trim_blocks=True)
-template = env.get_template('tpl.jinja')
-
-print(template.render(config))
-
+    env = Environment(loader=FileSystemLoader('./'), trim_blocks=True)
+    try:
+        template = env.get_template('fabric-tpl.jinja')
+        stack_data = template.render(config_data)
+        stack_fd.write(stack_data)
+    except TemplateNotFound as e:
+        print("{} Not Found".format(e))
