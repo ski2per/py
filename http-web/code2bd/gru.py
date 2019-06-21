3122ec93e5be1504bf863c22ad5b3e0efd9fc58a
zhangpeng <ski2per@163.com>
2019-06-21 17:33:02 +0800

    add code2bd

---
+++ http-web/code2bd/gru.py
@@ -0,0 +1,63 @@
+import json
+
+import tornado.web
+import tornado.websocket
+import tornado.httpserver
+import tornado.ioloop
+from tornado.escape import json_decode
+from redis import Redis, RedisError
+
+
+
+def get_minion_num():
+    try:
+        redis = Redis(host="localhost", port=6379, db=0)
+        num = redis.get("minions")
+    except RedisError:
+        num = -1
+
+    return 0 if not num else num
+
+
+class Index(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Online Minions: {}".format(get_minion_num()))
+
+    #def post(self):
+    #    print(self.request.headers)
+    #    print(self.request.body)
+    #    print(self.get_body_arguments("cpu"))
+    #    self.data = json_decode(self.request.body)
+    #    print(self.data)
+    #    self.write(json.dumps({"ok": "got it"}))
+
+class OnlineWS(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print("Minion online")
+
+    def on_message(self, message):
+        print(message)
+
+    def on_close(self):
+        print("Minion offline")
+
+
+class Gru(tornado.web.Application):
+    def __init__(self):
+
+        handlers = [
+            (r"/", Index),
+            (r"/ws", OnlineWS)
+        ]
+
+        settings = dict(
+            debug=True,
+            autoreload=True,
+        )
+
+        super(Gru, self).__init__(handlers, **settings)
+
+if __name__ == "__main__":
+    http_svr = tornado.httpserver.HTTPServer(Gru())
+    http_svr.listen(8000)
+    tornado.ioloop.IOLoop.current().start()
---
+++ http-web/code2bd/minion.py
@@ -0,0 +1,9 @@
+import socket
+import asyncio
+import websockets
+
+async def hello(uri):
+    async with websockets.connect(uri) as ws:
+        await ws.send("Hello from {}".format(socket.gethostname()))
+
+asyncio.get_event_loop().run_until_complete(hello('ws://localhost:8000/ws'))
