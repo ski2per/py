b71464e23ca15d8d86c7876e0aa72ab3f3f29d39
Ted <ski2per@163.com>
2019-07-15 17:21:08 +0800

    study concurrent

--- http-web/code2bd/foo.py
+++ http-web/code2bd/foo.py
@@ -1,15 +1,20 @@
 import asyncio
-import time
 
-async def say_after(delay, what):
-    await asyncio.sleep(delay)
-    print(what)
+
+async def count():
+    print("One")
+    await asyncio.sleep(1)
+    print("Two")
+
 
 async def main():
-    print(f"started at {time.strftime('%X')}")
-    await say_after(1, 'hello')
-    await say_after(2, 'world')
+    await asyncio.gather(count(), count(), count())
 
-    print(f"finished at {time.strftime('%X')}")
 
-asyncio.run(main())
+if __name__ == "__main__":
+    import time
+
+    s = time.perf_counter()
+    asyncio.run(main())
+    elapsed = time.perf_counter() - s
+    print(f"{__file__} executed in {elapsed:0.2f} seconds")
--- http-web/code2bd/minion.py
+++ http-web/code2bd/minion.py
@@ -1,12 +1,20 @@
+import time
 import socket
 import asyncio
 import websockets
 
 
-async def hello(uri):
+# async def hello(uri):
+#    async with websockets.connect(uri) as ws:
+#        await ws.send("Hello from {}".format(socket.gethostname()))
+
+async def main(uri):
     async with websockets.connect(uri) as ws:
-        await ws.send("Hello from {}".format(socket.gethostname()))
+        while True:
+            await ws.send("Hello from {}".format(socket.gethostname()))
+            time.sleep(5)
 
 
-asyncio.get_event_loop().run_until_complete(hello('ws://localhost:8000/ws'))
-asyncio.get_event_loop().run_forever()
+# asyncio.get_event_loop().run_until_complete(main('ws://localhost:8000/ws'))
+asyncio.get_event_loop().run_forever(main('ws://localhost:8000/ws'))
+# asyncio.get_event_loop().run_forever()
