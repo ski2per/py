ba16fe8a117a5b690948080883eb2dd20fd6a438
Ted <ski2per@163.com>
2019-07-15 17:40:32 +0800

    add example

--- http-web/code2bd/foo.py
+++ http-web/code2bd/foo.py
@@ -1,20 +1,40 @@
 import asyncio
 
+# async def count():
+#    print("One")
+#    await asyncio.sleep(1)
+#    print("Two")
+#
+#
+# async def main():
+#    await asyncio.gather(count(), count(), count())
+#
+#
+# if __name__ == "__main__":
+#    import time
+#
+#    s = time.perf_counter()
+#    asyncio.run(main())
+#    elapsed = time.perf_counter() - s
+#    print(f"{__file__} executed in {elapsed:0.2f} seconds")
 
-async def count():
+
+import time
+
+
+def count():
     print("One")
-    await asyncio.sleep(1)
+    time.sleep(1)
     print("Two")
 
 
-async def main():
-    await asyncio.gather(count(), count(), count())
+def main():
+    for _ in range(3):
+        count()
 
 
 if __name__ == "__main__":
-    import time
-
     s = time.perf_counter()
-    asyncio.run(main())
+    main()
     elapsed = time.perf_counter() - s
-    print(f"{__file__} executed in {elapsed:0.2f} seconds")
+    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
