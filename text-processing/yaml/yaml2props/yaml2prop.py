690deebd239525fff2860fcde1791b0c6c0db400
Ted <ski2per@163.com>
2019-08-15 15:48:35 +0800

    developing

--- text-processing/yaml/yaml2props/yaml2prop.py
+++ text-processing/yaml/yaml2props/yaml2prop.py
@@ -1,24 +1,25 @@
-import re
+import re 
 import os.path
+import argparse
 
 
 def yaml2prop(filename, space_num=2):
+    properties_file = '{}.properties'.format(filename)
     props = []
-    print(os.path.basename(filename))
-    with open(filename) as f:
+    with open(filename, 'r') as f,\
+        open(properties_file, 'w') as p:
         for line in f:
-
             # Ignore comments and blank line
             ignore = re.search(r'^\s?[#]', line)
             if ignore or not line.strip() or ':' not in line:
                 continue
 
+            # Get indent level
             ptn = re.compile(r'(\s{' + str(space_num) + r'})')
             indents = ptn.findall(line.split(':')[0])
-
             level = len(indents) if indents else 0
 
-            result_prop = re.search(r'.+(?=:\s)', line)
+            result_prop = re.search(r'.+(?=:\s?)', line)
 
             # Property without indent
             if level is 0:
@@ -36,9 +37,16 @@ def yaml2prop(filename, space_num=2):
             # Format value
             if value and value.group().strip():
                 new_line = '{} = {}'.format('.'.join(props), value.group().strip())
-                print(new_line)
+                print(new_line, file=p)
 
 
 if __name__ == '__main__':
-    filename = 'demo.yml'
-    yaml2prop(filename)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("yaml", help='Specify a YAML to convert')
+    args = parser.parse_args()
+
+    if not os.path.exists(args.yaml):
+        print("{} not exists".format(args.yaml))
+        exit(-1)
+
+    yaml2prop(args.yaml)
