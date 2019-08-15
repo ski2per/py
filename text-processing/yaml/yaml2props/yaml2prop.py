e0d1b32e3bf9233627d09a1696724f3fa2cdab2d
Ted <ski2per@163.com>
2019-08-15 14:07:14 +0800

    enhance yaml2prop.py

--- text-processing/yaml/yaml2props/yaml2prop.py
+++ text-processing/yaml/yaml2props/yaml2prop.py
@@ -1,20 +1,44 @@
 import re
+import os.path
 
 
-line = '      #shit abc'
-line = '  port: 8080'
-# line = 'server: '
-ignore = re.search(r'^\s?[#-]', line)
-print(ignore)
+def yaml2prop(filename, space_num=2):
+    props = []
+    print(os.path.basename(filename))
+    with open(filename) as f:
+        for line in f:
 
-tabs = re.findall(r'(\s\s)', line.split(':')[0])
-index = len(tabs) if tabs else 0
-print(tabs)
-print(index)
+            # Ignore comments and blank line
+            ignore = re.search(r'^\s?[#]', line)
+            if ignore or not line.strip() or ':' not in line:
+                continue
 
-result_prop = re.search(r'.+(?=:\s)', line)
-print(result_prop)
+            ptn = re.compile(r'(\s{' + str(space_num) + r'})')
+            indents = ptn.findall(line.split(':')[0])
 
-if ':' not in line:
-    pass
+            level = len(indents) if indents else 0
 
+            result_prop = re.search(r'.+(?=:\s)', line)
+
+            # Property without indent
+            if level is 0:
+                props = [result_prop.group().strip()]
+            # Property with indent
+            else:
+                prop_name = result_prop.group().strip()
+                while props and level < len(props):
+                    props.pop()
+                props.append(prop_name)
+
+            value = re.search(r'(?<=:).+', line)
+            # need to process if value is a list
+
+            # Format value
+            if value and value.group().strip():
+                new_line = '{} = {}'.format('.'.join(props), value.group().strip())
+                print(new_line)
+
+
+if __name__ == '__main__':
+    filename = 'demo.yml'
+    yaml2prop(filename)
