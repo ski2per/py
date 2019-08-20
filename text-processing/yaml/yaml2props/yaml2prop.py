68e63c16d0188f190966728985eec09c2f7b540e
Ted <ski2per@163.com>
2019-08-20 14:20:43 +0800

    finish yamp2prop.py

--- text-processing/yaml/yaml2props/yaml2prop.py
+++ text-processing/yaml/yaml2props/yaml2prop.py
@@ -1,16 +1,18 @@
-import re 
+import re
 import os.path
 import argparse
 
 
 def yaml2prop(filename, space_num=2):
     properties_file = '{}.properties'.format(filename)
+
+    # Store properties list
     props = []
     with open(filename, 'r') as f,\
-        open(properties_file, 'w') as p:
+            open(properties_file, 'w') as p:
         for line in f:
-            # Ignore comments and blank line
-            ignore = re.search(r'^\s?[#]', line)
+            # Ignore comments, blank line and line with dash('-')
+            ignore = re.search(r'^\s?[#-]', line)
             if ignore or not line.strip() or ':' not in line:
                 continue
 
@@ -19,25 +21,40 @@ def yaml2prop(filename, space_num=2):
             indents = ptn.findall(line.split(':')[0])
             level = len(indents) if indents else 0
 
-            result_prop = re.search(r'.+(?=:\s?)', line)
+            # Get property name
+            # Be careful about special character in line
+            prop_name = line.split(':')[0].strip()
 
-            # Property without indent
+            # If current indent is 0,
+            # initialize 'props' list with current property name
+            # (Empty old props list)
             if level is 0:
-                props = [result_prop.group().strip()]
-            # Property with indent
+                props = [prop_name]
             else:
-                prop_name = result_prop.group().strip()
+                # Adjust element in 'props' list according to indent level
                 while props and level < len(props):
                     props.pop()
                 props.append(prop_name)
 
-            value = re.search(r'(?<=:).+', line)
-            # need to process if value is a list
-
-            # Format value
-            if value and value.group().strip():
-                new_line = '{} = {}'.format('.'.join(props), value.group().strip())
-                print(new_line, file=p)
+            # Get property value
+            value_matched = re.search(r'(?<=:).+', line)
+            if value_matched:
+                prop_value = value_matched.group().strip()
+                if prop_value:
+                    # If text were literal list,
+                    # i.e.:
+                    # websvrs: ['192.168.100.1', '192.168.100.2']
+                    # It needs to be process as:
+                    # websrvs[0] = 192.168.100.1
+                    # websrvs[1] = 192.168.100.2
+                    if re.match(r'\[.+\]', prop_value):
+                        items = re.findall(r'[\"\'](.+?)[\"\']', prop_value)
+                        for i in range(len(items)):
+                            new_line = '{}[{}]={}'.format('.'.join(props), i, items[i])
+                            print(new_line, file=p)
+                    else:
+                        new_line = '{}={}'.format('.'.join(props), value_matched.group().strip())
+                        print(new_line, file=p)
 
 
 if __name__ == '__main__':
