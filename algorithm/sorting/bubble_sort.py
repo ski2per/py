a4fdb0f0a67783590496434631ffc1cdd89af988
Ted <ski2per@163.com>
2019-10-09 22:40:28 +0800

    update bubble sort

--- algorithm/sorting/bubble_sort.py
+++ algorithm/sorting/bubble_sort.py
@@ -1,20 +1,20 @@
-def bubble_sort(unsorted):
-    n = len(unsorted)
-    print(n)
-    for i in range(1, n):
-        # print(range(n-i-1))
-        for j in range(n-i):
-            # print("i={},j={}".format(i, j))
-            if unsorted[j] > unsorted[j+1]:
-                tmp = unsorted[j]
-                unsorted[j] = unsorted[j+1]
-                unsorted[j+1] = tmp
-                # unsorted[j], unsorted[j+1] = unsorted[j+1], unsorted[j]
-            print(unsorted)
+from random import randint
 
-    return unsorted
+
+def bubble_sort(arr):
+    n = len(arr)
+    # Loop through all element
+    for i in range(n):
+        # n -i - 1:
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                # Swap(Pythonic)
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
 
 
 if __name__ == '__main__':
     numbers = [2, 7, 4, 1, 5, 3]
+    nums = randint(1, 100)
+    print(nums)
     print(bubble_sort(numbers))
