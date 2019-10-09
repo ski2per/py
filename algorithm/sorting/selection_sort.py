e989eded3bf2658af1efb512dde1207d83782cb1
Ted <ski2per@163.com>
2019-10-09 23:51:32 +0800

    update selection sort

--- algorithm/sorting/selection_sort.py
+++ algorithm/sorting/selection_sort.py
@@ -1,21 +1,21 @@
-def selection_sort(unsorted):
-    n = len(unsorted)
+def selection_sort(arr):
+    n = len(arr)
 
-    for i in range(n - 1):
-        min_index = i  # assume 1st element is minimum
-        # scan to find the real minimum number
+    for i in range(n):
+        # Assume i element is minimum
+        min_index = i
+        # Scan to find the real minimum number
         for j in range(i + 1, n):
-            if unsorted[j] < unsorted[min_index]:
+            if arr[j] < arr[min_index]:
+                # Change minimum index to real minimum element of current round
                 min_index = j
+        # Swap real minimum element to i
+        arr[i], arr[min_index] = arr[min_index], arr[i]
 
-        tmp = unsorted[i]
-        unsorted[i] = unsorted[min_index]
-        unsorted[min_index] = tmp
-
-    return unsorted
+    return arr
 
 
 if __name__ == '__main__':
-    # numbers = [4, 2, 5, 7, 1, 3]
-    numbers = [7, 5, 4, 3, 2, 1]
+    numbers = [4, 2, 5, 7, 1, 3]
+    # numbers = [7, 5, 4, 3, 2, 1]
     print(selection_sort(numbers))
