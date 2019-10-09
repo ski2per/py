24b5f39a081b52b25b5502e13b0fc570888cb8ce
Ted <ski2per@163.com>
2019-10-10 01:23:56 +0800

    update insertion sort again

--- algorithm/sorting/insertion_sort.py
+++ algorithm/sorting/insertion_sort.py
@@ -7,22 +7,22 @@ def insertion_sort(arr):
         tmp = arr[i]
         slot = i
 
-        # Use while loop to index 0, and shift element greater than "tmp"
+        # Use while loop to shift element greater than "tmp"
         print(arr)
         while slot > 0 and arr[slot - 1] > tmp:
-            arr[slot], arr[slot-1] = arr[slot - 1], arr[slot]
+            arr[slot] = arr[slot - 1]
             print("{}, tmp:{}, slot:{}".format(arr, tmp, slot))
             slot -= 1
-        # arr[slot] = tmp
-        tmp = arr[slot]
+        # Insert
+        arr[slot] = tmp
+        print(slot)
         print(arr)
         print('----------------')
     return arr
 
 
 if __name__ == '__main__':
-    import random
-    # numbers = [7, 2, 4, 1, 5, 3]
-    numbers = [5, 4, 3, 2, 1, 0]
+    numbers = [7, 2, 4, 1, 5, 3]
+    # numbers = [5, 4, 3, 2, 1, 0]
     sorted_list = insertion_sort(numbers)
     print(sorted_list)
