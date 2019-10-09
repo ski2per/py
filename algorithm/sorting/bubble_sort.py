393fed9dc0e4dfa049baa2c1536f301509d4d284
Ted <ski2per@163.com>
2019-10-09 22:51:44 +0800

    update bubble sort again

--- algorithm/sorting/bubble_sort.py
+++ algorithm/sorting/bubble_sort.py
@@ -5,12 +5,17 @@ def bubble_sort(arr):
     n = len(arr)
     # Loop through all element
     for i in range(n):
-        # n -i - 1:
-        for j in range(n - i - 1):
+        # n-i-1:
+        # "n-i": Last i elements are already sorted
+        # "-1": The loop below will use "j+1", so "-1" is to limits the boundary
+        for j in range(n-i-1):
             if arr[j] > arr[j + 1]:
                 # Swap(Pythonic)
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
+
+def bubble_sort_optimized(arr):
+
 
 
 if __name__ == '__main__':
