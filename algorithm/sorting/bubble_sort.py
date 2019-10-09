d92e48d0850c7339eeb3ff6231e0c6fc55255255
Ted <ski2per@163.com>
2019-10-09 21:30:58 +0800

    update radar and bubble_sort

--- algorithm/sorting/bubble_sort.py
+++ algorithm/sorting/bubble_sort.py
@@ -1,13 +1,16 @@
 def bubble_sort(unsorted):
     n = len(unsorted)
+    print(n)
     for i in range(1, n):
         # print(range(n-i-1))
         for j in range(n-i):
+            # print("i={},j={}".format(i, j))
             if unsorted[j] > unsorted[j+1]:
                 tmp = unsorted[j]
                 unsorted[j] = unsorted[j+1]
                 unsorted[j+1] = tmp
-        print(unsorted)
+                # unsorted[j], unsorted[j+1] = unsorted[j+1], unsorted[j]
+            print(unsorted)
 
     return unsorted
 
