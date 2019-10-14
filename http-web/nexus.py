ee956e7ea4fed9b178f28f04802d1ae86fe053fe
Ted <ski2per@163.com>
2019-10-14 10:52:33 +0800

    update nexus.py

--- http-web/nexus.py
+++ http-web/nexus.py
@@ -1,8 +1,7 @@
 import re
-import json
-import time
 import shutil
 import requests
+import tempfile
 
 
 class Nexus:
@@ -14,7 +13,7 @@ class Nexus:
     def _fetch_url(self, url):
         response = requests.get(url, auth=(self.username, self.password))
         if not response.status_code == 200:
-            print("Error getting {}".format(url))
+            print('Error getting {}'.format(url))
             return {}
         else:
             return response.json()
@@ -23,7 +22,7 @@ class Nexus:
         repo_url = '{}/service/rest/v1/repositories'.format(self.base_url)
         return self._fetch_url(repo_url)
 
-    def list_components(self, repo_name, filter_regexp=r""):
+    def list_repo_components(self, repo_name, filter_regexp=r''):
         # Use 'filter_regexp' to filter component by name
         all_components = []
         components_url = '{}/service/rest/v1/components?repository={}'.format(self.base_url, repo_name)
@@ -46,6 +45,10 @@ class Nexus:
         else:
             return all_components
 
+    def list_component(self, component_id):
+        component_url = '{}/service/rest/v1/components/{}'.format(self.base_url, component_id)
+        return self._fetch_url(component_url)
+
     def delete_component(self, component_id):
         component_url = '{}/service/rest/v1/components/{}'.format(self.base_url, component_id)
         response = requests.delete(component_url, auth=(self.username, self.password))
@@ -54,47 +57,45 @@ class Nexus:
         else:
             print('Delete component({}) unsuccessfully'.format(component_id))
 
-    def list_component(self, component_id):
-        component_url = '{}/service/rest/v1/components/{}'.format(self.base_url, component_id)
-        return self._fetch_url(component_url)
-
     # To be optimized
     def transfer_maven_componemt(self):
+        tmp_jar = tempfile.TemporaryFile()
+
         source = 'maven-hosted'
-        components = self.list_components(source, r'rate')
-        cid = 'bWF2ZW4taG9zdGVkOjM2ZTNkZWM4ZGU1MjhjOWIyZmFiNTdlNDAyM2Y3YmI5'
-        component = self.list_component(cid)
-        components = [component]
+        # components = self.list_components(source, r'rate')
+        components = self.list_repo_components(source)
         for comp in components:
             source_url = comp['assets'][0]['downloadUrl']
             with requests.get(source_url, stream=True) as r:
-                with open('tmp.jar', 'wb') as f:
-                    shutil.copyfileobj(r.raw, f)
-            with open('tmp.jar', 'rb') as jar:
+                shutil.copyfileobj(r.raw, tmp_jar)
                 form_data = {
                     'maven2.groupId': (None, comp['group']),
                     'maven2.artifactId': (None, comp['name']),
                     'maven2.version': (None, comp['version']),
                     'maven2.generate-pom': (None, 'false'),
-                    'maven2.asset1': jar,
+                    'maven2.asset1': tmp_jar,
                     'maven2.asset1.extension': (None, 'jar')
                 }
-                r = requests.post('https://nexus.ted.mighty/service/rest/v1/components?repository=m2-hosted', verify=False,
-                                  auth=('user', 'user'), files=form_data)
-                print(r.status_code)
+                print('Transfering: {}.{}.{}'.format(comp['group'], comp['name'], comp['version']))
+
+                # Disable Self-signed TLS error
+                import urllib3
+                urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+                r = requests.post('https://nexus.ted.mighty/service/rest/v1/components?repository=m2-hosted',
+                                  verify=False, auth=('user', 'user'), files=form_data)
+                if r.status_code == 204:
+                    print("success")
 
 
 if __name__ == '__main__':
     nexus = Nexus()
+    nexus.transfer_maven_componemt()
+
     # components = nexus.list_components('zdsys', filter_regexp=r'.+\-building')
     # for component in components:
     #    print(component['id'])
     #    delete_component(component['id'])
     #    time.sleep(2)
-
-    nexus.transfer_maven_componemt()
-
-
 
 
     # foo = nexus.list_component(cid)
@@ -103,4 +104,3 @@ if __name__ == '__main__':
     # local_filename = url.split('/')[-1]
     #
     #
-
