c15f364e9169c322bbdf71c60e8ad05deb8ae83f
Ted <ski2per@163.com>
2019-10-14 23:23:23 +0800

    finish quiz 0

--- algorithm/leetcode/quiz0.py
+++ algorithm/leetcode/quiz0.py
@@ -5,7 +5,10 @@ class Solution:
     """
     Given an array of integers, return indices of the two numbers such that they add up to a specific target.
     You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    Given nums = [2, 7, 11, 15], target = 9,
 
+    Because nums[0] + nums[1] = 2 + 7 = 9,
+    return [0, 1].
     """
     def two_sum(self, nums, target):
         """
@@ -13,11 +16,12 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-
-        d = {}
+        n = len(nums)
         for idx, num in enumerate(nums):
-            pass
-
+            remainder = target - num
+            for x in range(idx+1, n):
+                if nums[x] == remainder:
+                    return [idx, x]
 
 
 if __name__ == '__main__':
