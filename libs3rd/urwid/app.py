49858665f5b403e0fe32b04d2c9c40561b30e0b8
Ted <ski2per@163.com>
2020-02-25 23:11:12 +0800

    sleep

--- libs3rd/urwid/app.py
+++ libs3rd/urwid/app.py
@@ -1,14 +1,24 @@
 import urwid
 
+
+class BasicView(urwid.WidgetWrap):
+
+    def __int__(self):
+        self.body = urwid.AttrWrap(urwid.Text('Empty body'), 'body')
+        self.view = urwid.Frame(self.body, header=self.header, footer=self.footer)
+        urwid.WidgetWrap.__init__(self, self.view)
+
+
+
 class PackageChoice(urwid.WidgetWrap):
     def __init__(self):
         self.options = []
-        list_content = [
+        content = [
             urwid.RadioButton(self.options, u"Unsure"),
             urwid.RadioButton(self.options, u"Yes"),
             urwid.RadioButton(self.options, u"No"),
         ]
-        list_box = urwid.LineBox(urwid.SimpleListWalker(list_content))
+        list_box = urwid.ListBox(urwid.SimpleListWalker(content))
 
         urwid.WidgetWrap.__init__(self, list_box)
 
@@ -17,66 +27,85 @@ class PackageChoice(urwid.WidgetWrap):
             if o.get_state() is True:
                 return o.get_label()
 
-class Index(urwid.WidgetWrap):
-    text_intro = [('important', u"Text"),
-                  u" widgets are the most common in "
-                  u"any urwid program.  This Text widget was created "
-                  u"without setting the wrap or align mode, so it "
-                  u"defaults to left alignment with wrapping on space "
-                  u"characters.  ",
-                  ('important', u"Change the window width"),
-                  u" to see how the widgets on this page react.  "
-                  u"This Text widget is wrapped with a ",
-                  ('important', u"Padding"),
-                  u" widget to keep it indented on the left and right."]
+
+class StartView(urwid.WidgetWrap):
+    text_intro = [
+        ('important', 'Valar Morghulis'),
+        ' is a Braavosi greeting said in the High Valyrian language,',
+        ' which literally translates to ',
+        ('important', "All men must die"),
+        ' in the Common Tongue. ',
+        ('important', 'Valar Dohaeris'),
+        ' is its accompanying greeting, which literally translates to ',
+        ('important', "All men must serve"),
+        "."
+    ]
 
     def __init__(self):
         text = urwid.Text(self.text_intro)
-        listbox_content = [
-            urwid.Padding(text, left=2, right=2, min_width=20),
-        ]
-        listbox = urwid.ListBox(urwid.SimpleListWalker(listbox_content))
-        urwid.WidgetWrap.__init__(self, listbox)
 
+        listbox_content = [
+            urwid.Divider(top=6),
+            urwid.Padding(text, align='center', width=('relative', 80)),
+            urwid.Divider(),
+        ]
+        body_content = urwid.ListBox(urwid.SimpleListWalker(listbox_content))
+
+        body = urwid.AttrWrap(body_content, 'body')
+        super().__init__(body)
 
 
 
 class SetupWizard:
     palette = [
-        ('body','black','light gray', 'standout'),
-        ('reverse','light gray','black'),
-        ('head','white','dark red', 'bold'),
-        ('important','dark blue','light gray',('standout','underline')),
-        ('editfc','white', 'dark blue', 'bold'),
-        ('editbx','light gray', 'dark blue'),
-        ('editcp','black','light gray', 'standout'),
-        ('bright','dark gray','light gray', ('bold','standout')),
-        ('foot','black','dark cyan'),
-        ('buttnf','white','dark blue','bold'),
+        ('header', 'white', 'dark green', 'bold'),
+        ('body', 'default', 'default'),
+        ('footer', 'default', 'dark blue'),
+        ('reverse', 'light gray', 'black'),
+        ('important', 'dark blue', 'light gray', ('standout', 'underline')),
+        ('key', 'yellow', 'dark green'),
+        ('editfc', 'white', 'dark green', 'bold'),
+        ('editbx', 'light gray', 'dark blue'),
+        ('editcp', 'black', 'light gray', 'standout'),
+        ('bright', 'dark gray', 'light gray', ('bold', 'standout')),
+        ('buttnf', 'white', 'dark blue', 'bold'),
     ]
 
-    header_text = ('head', [
+    header_text = ('header', [
         "XXXXX Offline Setup Wizard",
     ])
 
-    footer_text = ('foot', [
-        ('key', "  F2"), " Continue   ",
-        ('key', "F4"), " Quit",
+    footer_text = ('footer', [
+        " Continue", ('key', " F2 "),
+        "    Quit", ('key', " F4 "),
     ])
 
     def __init__(self):
-        self.package_choice = PackageChoice()
-        self.header = urwid.AttrWrap(urwid.Text(self.header_text), 'head')
-        self.footer = urwid.AttrWrap(urwid.Text(self.footer_text), "foot")
-        self.view = urwid.Frame(urwid.AttrWrap(Index(), 'body'),
-                                header=self.header,
-                                footer=self.footer)
+        self.last_body = None
+        self.header = urwid.AttrWrap(urwid.Text(self.header_text), 'header')
+        self.footer = urwid.AttrWrap(urwid.Text(self.footer_text), 'footer')
+        self.current_body = StartView()
+        view = urwid.Frame(self.current_body, header=self.header, footer=self.footer)
+
+        self.loop = urwid.MainLoop(view, self.palette, unhandled_input=self.unhandled_keypress)
+
+    def change_view(self, w):
+        self.last_body = self.current_body
+        self.current_body = w
+        view = urwid.Frame(self.current_body, header=self.header, footer=self.footer)
+        self.loop.widget = view
+
 
     def unhandled_keypress(self, k):
         """Last resort for keypresses."""
 
         if k == "f2":
-            self.loop.widget = self.package_choice
+            # self.loop.widget = self.package_choice
+            package_choice = PackageChoice()
+            self.change_view(package_choice)
+        elif k == "f3":
+            if self.last_body:
+                self.change_view(self.last_body)
         elif k == "f4":
             raise urwid.ExitMainLoop()
         else:
@@ -84,10 +113,8 @@ class SetupWizard:
         return True
 
     def main(self):
-        self.loop = urwid.MainLoop(self.view, self.palette, unhandled_input=self.unhandled_keypress)
         self.loop.run()
+
 
 if __name__ == '__main__':
     SetupWizard().main()
-
-
