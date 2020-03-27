b2acdb93c7a2e08060a20cf43fbf3ed9a32ce615
Ted <ski2per@163.com>
2020-03-27 16:03:03 +0800

    update basic_logging.py

--- abc/logging/basic_logging.py
+++ abc/logging/basic_logging.py
@@ -8,3 +8,17 @@ logging.basicConfig(level=logging.DEBUG)
 logging.info("info")
 logging.debug("debug")
 
+def logger(name = 's1decar'):
+    log = logging.getLogger(name)
+
+    file_handler = logging.FileHandler('/tmp/s1decar.log')
+    log_format = '[%(asctime)s] %(levelname)s [%(threadName)s]: %(message)s'
+    date_format = '%Y-%m-%d %H:%M:%S'
+    # formatter = logging.Formatter(fmt=log_format, datefmt=date_format)
+    formatter = logging.Formatter(fmt=log_format)
+    file_handler.setFormatter(formatter)
+    # logging.basicConfig(format=log_format, datefmt=date_format)
+
+    log.addHandler(file_handler)
+    log.setLevel(logging.INFO)
+    return log
