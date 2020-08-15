fc3fc45e7ecf1a62b9146553a553bf0f0248244f
Ted <ski2per@163.com>
2020-08-15 10:05:08 +0800

    work work

--- abc/oop/inheritance/multi.py
+++ abc/oop/inheritance/multi.py
@@ -3,9 +3,25 @@ class A:
         self.name = name
 
     def hi(self):
-        print(f"Hi, this is {self.name}")
+        print(f"Hi, {self.name}, from A")
 
+class B:
+    def __init__(self, name):
+        self.name = name
+
+    def hi(self):
+        print(f"Hi, {self.name}, from B")
+
+
+class C(B, A):
+    def __init__(self, name):
+        super().__init__(name)
+        self.name = name
+
+    def hi(self):
+        super(B, self).hi()
+        print(self.name)
 
 if __name__ == "__main__":
-    a = A("apple")
-    a.hi()
+    c = C("shit")
+    c.hi()
