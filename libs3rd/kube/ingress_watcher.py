a8ebec446d0175c25a3f8cc7257367c62db0fe61
Ted <ski2per@163.com>
2020-12-21 15:58:18 +0800

    halt, use * configuration for bind9

--- libs3rd/kube/ingress_watcher.py
+++ libs3rd/kube/ingress_watcher.py
@@ -1,11 +1,14 @@
+import subprocess
 from kubernetes import client, config, watch
 config.load_kube_config()
 
-bind9_file_map = {
+BIND9_FILE_MAP = {
     "cetcxl.local": "db.cetcxl.local",
     "dev.cetcxl.local": "db.dev.cetcxl.local",
     "devops.cetcxl.local": "db.devops.cetcxl.local",
 }
+INGRESS_ADDR = "192.168.2.53"
+
 
 def extract_host_domain(text: str) -> tuple:
     return tuple(text.split(".", 1))
@@ -24,4 +27,11 @@ for event in watcher.stream(networking_api.list_ingress_for_all_namespaces):
         for rule in event_obj_spec.rules:
             if rule.host.endswith("cetcxl.local"):
                 host, domain = extract_host_domain(rule.host)
-                print(event_type, host,  bind9_file_map[domain])
+                zone_file = BIND9_FILE_MAP[domain]
+
+                if event_type == "added":
+                    print(f"bash dns-entry-tool.sh -t add -f {zone_file} -n {host} -i {INGRESS_ADDR}")
+                else:
+                    print(f"bash dns-entry-tool.sh -t del -f {zone_file} -n {host}")
+
+
