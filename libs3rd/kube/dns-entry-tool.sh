1f68f7f532c2246342bff84d44f597c925c75a6f
Ted <ski2per@163.com>
2020-12-21 15:27:06 +0800

    finish dns-entry-tool.sh

--- libs3rd/kube/dns-entry-tool.sh
+++ libs3rd/kube/dns-entry-tool.sh
@@ -7,7 +7,8 @@ function usage {
 Usage: $(basename "$0") [OPTION]...
   -t    Action type: <add|del>
   -f    Bind9 DNS zone file
-  -n    hostname(without domain suffix)
+  -n    A record(without domain suffix)
+  -i    IP address of A record specified in "-n"
   -h    display help
 
 EOF
@@ -19,72 +20,39 @@ function log {
     /bin/date +"[%Y/%m/%d %H:%M:%S]: $msg" >> $LOG
 }
 
-function take_sanpshot {
-    # $1: dataset name
+function add_A_record {
+    # $1: zone file
+    # $2: hostname
+    # $3: IP address
 
-    #now=$(date +"%Y%m%d-%H%M")
-    now=$(date +"%Y%m%d-%H%M%S")
-    log "[take_snapshot] Snapshot: $ZFS_POOL/$1@$now"
-    zfs snapshot $ZFS_POOL/$1@$now
-}
+    if [ -e "$1" ];then
+        a_record_tpl="$2\t\tIN\tA\t$3"
 
-function clean_snapshot {
-    # $1: dataset name
-
-    for snap in $(zfs list -t snap -H -p -s creation -o name | grep $ZFS_POOL/$1 | head -n -$KEEP_SNAPSHOT);do
-        log "[INFO] clean snapshot: $snap"
-        zfs destroy $snap
-    done
-}
-
-function check_backup {
-    # $1: dataset name
-    # $2: latest snapshot
-
-    latest_backup=$(ssh $ZFS_REMOTE_LOGIN zfs list -t snap -H -p -s creation -o name | grep "$HOST-$1" | tail -1)
-    latest_backup_ts=${latest_backup##*@}
-    latest_snapshot_ts=${2##*@}
-
-    if [ "$latest_snapshot_ts" == "$latest_backup_ts" ];then
-        log "[INFO] latest_snapshot_ts($latest_snapshot_ts)==latest_backup_ts($latest_backup_ts), NO NEED TO BACKUP($1)."
-        FLAG=1
+        grep -E "$2\t" $1 &> /dev/null
+        if [ "$?" -eq 0 ];then
+            echo "Record $2 already exists"
+        else
+            echo -e "$a_record_tpl" >> $1
+        fi
     else
-        FLAG=0
+        echo "Zone fle $1 not found !"
     fi
 }
 
-function send_init_snapshot {
-    # $1: dataset name
-    # $2: latest snapshot
+function del_A_record {
+    # $1: zone file
+    # $2: hostname
 
-    backup_name="$HOST-$1"
-
-    log "[send_init_snapshot] Send: $2"
-    zfs send $2 | ssh $ZFS_REMOTE_LOGIN zfs recv -F $ZFS_REMOTE_POOL/$backup_name
-    ret=$?
-    if [ $ret -ne 0 ];then
-        log "[ERROR] send_init_snapshot failed, CHECK DATASET ON BACKUP HOST($1)"
-    fi
-}
-
-function send_incremental_snapshot {
-    # $1: dataset name
-
-    latest_snapshot=($(zfs list -t snap -H -p -s creation -o name | grep "$ZFS_POOL/$1" | tail -1))
-    latest_backup=$(ssh $ZFS_REMOTE_LOGIN zfs list -t snap -H -p -s creation -o name | grep "$HOST-$1")
-    if [ -z "$latest_backup" ];then
-        log "[INFO] NO BACKUP FOR DATASET($1), send_init_snapshot"
-        send_init_snapshot $1 $latest_snapshot
+    if [ -e "$1" ];then
+        sed -i "/$2\t/d" $1
     else
-        log "[send_incremental_snapshot] Send: $ZFS_POOL/$1@$latest_backup_ts -> $latest_snapshot"
-        backup_name="$HOST-$1"
-        zfs send -i $ZFS_POOL/$1@$latest_backup_ts $latest_snapshot | ssh $ZFS_REMOTE_LOGIN zfs recv -F $ZFS_REMOTE_POOL/$backup_name
+        echo "Zone fle $1 not found !"
     fi
-
 }
+
 
 # Parse command line arguments
-while getopts ":t:f:n:h" opt; do
+while getopts ":t:f:n:i:h" opt; do
     case "$opt" in
         t)
             TYPE=${OPTARG}
@@ -96,6 +64,9 @@ while getopts ":t:f:n:h" opt; do
         n)
             HOSTNAME=$OPTARG
             ;;
+        i)
+            A_RECORD=$OPTARG
+            ;;
         h|*)
             usage
             ;;
@@ -104,7 +75,7 @@ done
 #shift $((OPTIND-1))
 
 if [ "$TYPE" == "add" ];then
-    if [ -z "${ZONE_FILE}" ] || [ -z "${HOSTNAME}" ];then
+    if [ -z "${ZONE_FILE}" ] || [ -z "${HOSTNAME}" ] || [ -z "${A_RECORD}" ];then
         usage
     fi
 elif [ "$TYPE" == "del" ];then
@@ -125,9 +96,9 @@ HOST=$(hostname)
 
 
 if [ "$TYPE" == "add" ];then
-    echo "add $ZONE_FILE $HOSTNAME"
+    add_A_record $ZONE_FILE $HOSTNAME $A_RECORD
 elif [ "$TYPE" == "del" ];then
-    echo "del $ZONE_FILE $HOSTNAME"
+    del_A_record $ZONE_FILE $HOSTNAME
 else
     usage
 fi
--- libs3rd/kube/ingress_watcher.py
+++ libs3rd/kube/ingress_watcher.py
@@ -1,6 +1,15 @@
 from kubernetes import client, config, watch
 config.load_kube_config()
 
+bind9_file_map = {
+    "cetcxl.local": "db.cetcxl.local",
+    "dev.cetcxl.local": "db.dev.cetcxl.local",
+    "devops.cetcxl.local": "db.devops.cetcxl.local",
+}
+
+def extract_host_domain(text: str) -> tuple:
+    return tuple(text.split(".", 1))
+
 
 v1_api = client.CoreV1Api()
 networking_api = client.NetworkingV1beta1Api()
@@ -14,4 +23,5 @@ for event in watcher.stream(networking_api.list_ingress_for_all_namespaces):
     if event_type in ["added", "deleted"]:
         for rule in event_obj_spec.rules:
             if rule.host.endswith("cetcxl.local"):
-                print(event_type, rule.host)
+                host, domain = extract_host_domain(rule.host)
+                print(event_type, host,  bind9_file_map[domain])
